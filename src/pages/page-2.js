464fcf40f6751fddec91281f06ec1bf5df88a1de
Rakindu Marambe <116582517+RakinduM@users.noreply.github.com>
2023-02-07 01:55:38 +0530

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,7 @@ import {Seo} from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <div className="container text-center my-5">
-      <h1>Hi from the second page</h1>
+      <h1>Helloo from the second page</h1>
       <p>Welcome to page 2</p>
       <Link to="/">Go back to the homepage</Link>
     </div>
